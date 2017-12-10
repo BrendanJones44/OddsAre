c3aafefe803b57d6ed4b449356cc82d21d874b28
BrendanJones44 <bpj1651@rit.edu>
2017-12-09 20:42:59 -0500

    added functionality to make notifications go away

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -1,7 +1,20 @@
 $(function() {
 	$('.dismiss-notification').on("click", function(event){
+		var notificationElement = $(this);
+		var parent = $(this).parent().parent();
+		console.log(parent);
     event.preventDefault();
-    console.log("happened!");
+		$.ajax({
+			url: $('.dismiss-notification').attr("url"),
+			type: 'POST',
+			success:function(){
+				parent.remove();
+				var numberOfNotifications = Number($('#notification-count').text()) - 1;
+				if(numberOfNotifications === 0){
+					$('#notification-dropdown').append( "<a class='dropdown-item' href=''>No new notifications</a>" );
+				}
+				$('#notification-count').text(numberOfNotifications);
+			}
+		})
   })
 });
-console.log("happened");
