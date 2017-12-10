cf5764049e9cd0806dffc2d7d98ef6d3fa56d8c9
BrendanJones44 <bpj1651@rit.edu>
2017-12-10 17:34:22 -0500

    bugfixes and styling to notifications dropdown

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -1,20 +1,22 @@
-$(function() {
-	$('.dismiss-notification').on("click", function(event){
-		var notificationElement = $(this);
-		var parent = $(this).parent().parent();
-		console.log(parent);
-    event.preventDefault();
-		$.ajax({
-			url: $('.dismiss-notification').attr("url"),
-			type: 'POST',
-			success:function(){
-				parent.remove();
-				var numberOfNotifications = Number($('#notification-count').text()) - 1;
-				if(numberOfNotifications === 0){
-					$('#notification-dropdown').append( "<a class='dropdown-item' href=''>No new notifications</a>" );
+document.addEventListener("turbolinks:load", function() {
+	$(function() {
+		$('.dismiss-notification').on("click", function(event){
+			var notificationElement = $(this);
+			var parent = $(this).parent().parent();
+			console.log(parent);
+	    event.preventDefault();
+			$.ajax({
+				url: $('.dismiss-notification').attr("url"),
+				type: 'POST',
+				success:function(){
+					parent.remove();
+					var numberOfNotifications = Number($('#notification-count').text()) - 1;
+					if(numberOfNotifications === 0){
+						$('#notification-dropdown').append( "<a class='dropdown-item' href=''>No new notifications</a>" );
+					}
+					$('#notification-count').text(numberOfNotifications);
 				}
-				$('#notification-count').text(numberOfNotifications);
-			}
-		})
-  })
-});
+			})
+	  })
+	});
+})
