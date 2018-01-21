0b9126cc5d95ae7dc8d06487159fe6fedc50b064
BrendanJones44 <bpj1651@rit.edu>
2018-01-21 10:38:04 -0500

    Updated friendship controller to account for preconditions failing

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -16,10 +16,19 @@ class FriendshipController < ApplicationController
 
   # POST /friendship/accept_friend_request
   def accept_friend_request
-    @actor = User.find_by_user_name(params[:user_name])
-    current_user.accept_request(@actor)
-    Notification.create(recipient: @actor, actor: current_user, action: "accepted your friend request", notifiable: current_user, dismiss_type: "on_click")
-    redirect_back(fallback_location: root_path)
+    actor = User.find(params.require(:user_id))
+    if not actor.blank?
+      if current_user.friends.include? actor
+        raise Exception.new('Friendship already exists')
+      end
+      if actor.pending_friends.include? current_user
+        current_user.accept_request(actor)
+        Notification.create(recipient: actor, actor: current_user, action: "accepted your friend request", notifiable: current_user, dismiss_type: "on_click")
+      else
+        raise Exception.new('No friend request exists to accept')
+      end
+    end
+      redirect_back(fallback_location: root_path)
   end
 
   # GET /friendship/show_friend_requests
--- spec/controllers/friendship_controller_spec.rb
+++ spec/controllers/friendship_controller_spec.rb
@@ -65,4 +65,82 @@ RSpec.describe FriendshipController, type: :controller do
       end
     end
   end
+
+  describe "POST #accept_friend_request" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    context "anonymous user" do
+      it "should be redirected to signin" do
+        post :accept_friend_request
+        expect( response ).to redirect_to( new_user_session_path )
+      end
+    end
+
+    context "authenticated user" do
+      before do
+        sign_in user
+      end
+
+      context "requests with no parameters" do
+        it "raises an exception" do
+          expect{ post(:accept_friend_request, {}) }.to raise_error ActionController::ParameterMissing
+        end
+      end
+
+      context "requests with non-existent user_id" do
+        it "raises an exception" do
+          expect{ post :send_friend_request, params: { user_id: 10000 } }.to raise_error ActiveRecord::RecordNotFound
+        end
+      end
+
+      context "requests with existing alternate user" do
+        let(:other_user) { FactoryGirl.create :user }
+
+        context "and accepting user didn't receive a friend request from other user" do
+          it "should respond with an error" do
+            expect{ post :accept_friend_request, params: { user_id: other_user.id }}. to raise_error 'No friend request exists to accept'
+          end
+        end
+
+        context "and accepting user is already friends with other user" do
+          before do
+            user.friend_request(other_user)
+            other_user.accept_request(user)
+          end
+
+          it "should respond with an error" do
+            expect{ post :accept_friend_request, params: { user_id: other_user.id }}. to raise_error 'Friendship already exists'
+          end
+        end
+
+        context "and accepting user sent the user a friend request" do
+          before do
+            request.env["HTTP_REFERER"] = "base_path"
+            other_user.friend_request(user)
+            post :accept_friend_request, params: { user_id: other_user.id }
+          end
+
+          it "should redirect user back" do
+            expect( response ).to redirect_to "base_path"
+          end
+
+          it "should have updated other user's friend requests" do
+            expect(other_user.requested_friends.size).to eql 0
+          end
+
+          it "should have sent the other user a notification" do
+            expect(other_user.notifications.size).to eql 1
+          end
+
+          it "should recognize other_user as a friend" do
+            expect(user.friends.include? other_user). to eql true
+          end
+
+          it "should recognize user as other_user's friend" do
+            expect(other_user.friends.include? user) . to eql true
+          end
+        end
+      end
+    end
+  end
 end
