0bedb6c71737c3c49f930a87b1c476b110ff2022
BrendanJones44 <bpj1651@rit.edu>
2018-05-02 18:34:34 -0400

    significant refactoring to user model tests

---
+++ spec/support/odds_are_helpers.rb
@@ -0,0 +1,106 @@
+module OddsAreHelpers
+  def get_odds_are_with_no_response
+    user_a = FactoryGirl.create :user
+    user_b = FactoryGirl.create :user
+
+    user_a.friend_request(user_b)
+    user_b.accept_request(user_a)
+
+    challenge_request = FactoryGirl.create(:challenge_request)
+    FactoryGirl.create(:odds_are,
+                       :initiator => user_a,
+                       :recipient => user_b,
+                       :challenge_request => challenge_request)
+  end
+
+  def get_odds_are_with_no_finalization
+    user_a = FactoryGirl.create :user
+    user_b = FactoryGirl.create :user
+
+    user_a.friend_request(user_b)
+    user_b.accept_request(user_a)
+
+    challenge_request = FactoryGirl.create(:challenge_request)
+
+    challenge_response = FactoryGirl.create(:challenge_response,
+                                            :odds_are_id => 1,
+                                            :number_chosen => 10,
+                                            :odds_out_of => 50)
+
+    FactoryGirl.create(:odds_are,
+                       :initiator => user_a,
+                       :recipient => user_b,
+                       :challenge_request => challenge_request,
+                       :challenge_response => challenge_response,
+                       :responded_to_at => Time.zone.now)
+  end
+
+  def get_odds_are_with_winner
+    user_a = FactoryGirl.create :user
+    user_b = FactoryGirl.create :user
+
+    user_a.friend_request(user_b)
+    user_b.accept_request(user_a)
+
+    challenge_request = FactoryGirl.create(:challenge_request)
+
+    challenge_response = FactoryGirl.create(:challenge_response,
+                                            :odds_are_id => 1,
+                                            :number_chosen => 10,
+                                            :odds_out_of => 50)
+
+    challenge_finalization = FactoryGirl.create(:challenge_finalization,
+                                                :odds_are_id => 1,
+                                                :number_guessed => 40)
+
+    task = FactoryGirl.create(:task,
+                              :winner => user_b,
+                              :loser => user_a)
+
+    FactoryGirl.create(:odds_are,
+                        :initiator => user_a,
+                        :recipient => user_b,
+                        :challenge_request => challenge_request,
+                        :challenge_response => challenge_response,
+                        :responded_to_at => Time.zone.now,
+                        :challenge_finalization =>
+                          challenge_finalization,
+                        :finalized_at => Time.zone.now,
+                        :task => task)
+  end
+
+  def get_odds_are_with_no_winner
+    user_a = FactoryGirl.create :user
+    user_b = FactoryGirl.create :user
+
+    user_a.friend_request(user_b)
+    user_b.accept_request(user_a)
+
+    challenge_request = FactoryGirl.create(:challenge_request)
+
+    challenge_response = FactoryGirl.create(:challenge_response,
+                                            :odds_are_id => 1,
+                                            :number_chosen => 10,
+                                            :odds_out_of => 50)
+
+    challenge_finalization = FactoryGirl.create(:challenge_finalization,
+                                                :odds_are_id => 1,
+                                                :number_guessed => 2)
+
+    FactoryGirl.create(:odds_are,
+                       :initiator => user_a,
+                       :recipient => user_b,
+                       :challenge_request => challenge_request,
+                       :challenge_response => challenge_response,
+                       :responded_to_at => Time.zone.now,
+                       :challenge_finalization =>
+                         challenge_finalization,
+                       :finalized_at => Time.zone.now )
+
+  end
+  module_function :get_odds_are_with_no_response
+  module_function :get_odds_are_with_no_finalization
+  module_function :get_odds_are_with_winner
+  module_function :get_odds_are_with_no_winner
+
+end
--- spec/support/user_helpers.rb
+++ spec/support/user_helpers.rb
@@ -1,3 +1,5 @@
+require_relative './odds_are_helpers'
+
 module UserHelpers
   def get_user_with_friends
     user_a = FactoryGirl.create :user
@@ -15,171 +17,48 @@ module UserHelpers
   end
 
   def get_user_with_complete_odds_are_where_nobody_wins
-    get_user_with_complete_odds_are_where_nobody_wins_as_initiator(true)
-  end
-
-  def get_user_with_complete_odds_are_where_nobody_wins_as_initiator(initiator)
-    user_a = FactoryGirl.create :user
-    user_b = FactoryGirl.create :user
-
-    user_a.friend_request(user_b)
-    user_b.accept_request(user_a)
-
-    challenge_request = FactoryGirl.create(:challenge_request)
-
-    challenge_response = FactoryGirl.create(:challenge_response,
-                                            :odds_are_id => 1,
-                                            :number_chosen => 10,
-                                            :odds_out_of => 50)
-
-    challenge_finalization = FactoryGirl.create(:challenge_finalization,
-                                                :odds_are_id => 1,
-                                                :number_guessed => 2)
-
-    odds_are = FactoryGirl.create(:odds_are,
-                                  :initiator => user_a,
-                                  :recipient => user_b,
-                                  :challenge_request => challenge_request,
-                                  :challenge_response => challenge_response,
-                                  :responded_to_at => Time.zone.now,
-                                  :challenge_finalization =>
-                                    challenge_finalization,
-                                  :finalized_at => Time.zone.now )
-
-    if initiator
-      user_a
-    else
-      user_b
-    end
+    OddsAreHelpers.get_odds_are_with_no_winner.initiator
   end
 
   def get_user_with_unresponded_odds_are
-    get_user_with_unresponded_odds_are_as_initiator(true)
-  end
-
-  def get_user_with_unresponded_odds_are_as_initiator(initiator)
-    user_a = FactoryGirl.create :user
-    user_b = FactoryGirl.create :user
-
-    user_a.friend_request(user_b)
-    user_b.accept_request(user_a)
-
-    challenge_request = FactoryGirl.create(:challenge_request)
-    odds_are = FactoryGirl.create(:odds_are,
-                                  :initiator => user_a,
-                                  :recipient => user_b,
-                                  :challenge_request => challenge_request)
-
-    if initiator
-      user_a
-    else
-      user_b
-    end
+    OddsAreHelpers.get_odds_are_with_no_response.initiator
   end
 
   def get_user_with_unfinalized_odds_are
-    get_user_with_unfinalized_odds_are_as_initiator(true)
-  end
-
-
-  def get_user_with_unfinalized_odds_are_as_initiator(initiator)
-    user_a = FactoryGirl.create :user
-    user_b = FactoryGirl.create :user
-
-    user_a.friend_request(user_b)
-    user_b.accept_request(user_a)
-
-    challenge_request = FactoryGirl.create(:challenge_request)
-
-    challenge_response = FactoryGirl.create(:challenge_response,
-                                            :odds_are_id => 1,
-                                            :number_chosen => 10,
-                                            :odds_out_of => 50)
-
-    odds_are = FactoryGirl.create(:odds_are,
-                                  :initiator => user_a,
-                                  :recipient => user_b,
-                                  :challenge_request => challenge_request,
-                                  :challenge_response => challenge_response,
-                                  :responded_to_at => Time.zone.now)
-    if initiator
-      user_a
-    else
-      user_b
-    end
-  end
-
-  def get_user_with_complete_odds_are_with_winner(winner)
-    user_a = FactoryGirl.create :user
-    user_b = FactoryGirl.create :user
-
-    user_a.friend_request(user_b)
-    user_b.accept_request(user_a)
-
-    challenge_request = FactoryGirl.create(:challenge_request)
-
-    challenge_response = FactoryGirl.create(:challenge_response,
-                                            :odds_are_id => 1,
-                                            :number_chosen => 10,
-                                            :odds_out_of => 50)
-
-    challenge_finalization = FactoryGirl.create(:challenge_finalization,
-                                                :odds_are_id => 1,
-                                                :number_guessed => 40)
-
-    task = FactoryGirl.create(:task,
-                              :winner => user_b,
-                              :loser => user_a)
-
-    odds_are = FactoryGirl.create(:odds_are,
-                                  :initiator => user_a,
-                                  :recipient => user_b,
-                                  :challenge_request => challenge_request,
-                                  :challenge_response => challenge_response,
-                                  :responded_to_at => Time.zone.now,
-                                  :challenge_finalization =>
-                                    challenge_finalization,
-                                  :finalized_at => Time.zone.now,
-                                  :task => task)
-
-    if winner
-      user_b
-    else
-      user_a
-    end
+    OddsAreHelpers.get_odds_are_with_no_finalization.initiator
   end
 
   def get_user_with_won_odds_are
-    get_user_with_complete_odds_are_with_winner(true)
+    OddsAreHelpers.get_odds_are_with_winner.task.winner
   end
 
   def get_user_with_lost_odds_are
-    get_user_with_complete_odds_are_with_winner(false)
+    OddsAreHelpers.get_odds_are_with_winner.task.loser
   end
 
   def get_user_initiating_odds_are_with_no_response
-    get_user_with_unresponded_odds_are_as_initiator(true)
+    OddsAreHelpers.get_odds_are_with_no_response.initiator
   end
 
   def get_user_initiating_odds_are_with_no_finalization
-    get_user_with_unfinalized_odds_are_as_initiator(true)
+    OddsAreHelpers.get_odds_are_with_no_finalization.initiator
   end
 
   def get_user_initiating_odds_are_that_is_complete
-    get_user_with_complete_odds_are_where_nobody_wins_as_initiator(true)
+    OddsAreHelpers.get_odds_are_with_no_winner.initiator
   end
 
   def get_user_receiving_odds_are_with_no_response
+    OddsAreHelpers.get_odds_are_with_no_response.recipient
     get_user_with_unresponded_odds_are_as_initiator(false)
   end
 
   def get_user_receiving_odds_are_with_no_finalization
+    OddsAreHelpers.get_odds_are_with_no_finalization.recipient
     get_user_with_unfinalized_odds_are_as_initiator(false)
   end
 
   def get_user_receiving_odds_are_that_is_complete
-    get_user_with_complete_odds_are_where_nobody_wins_as_initiator(false)
+    OddsAreHelpers.get_odds_are_with_no_winner.recipient
   end
-
-
 end
