b3acd3f2730d709b52e3400b7150fd1cebb8a6d7
BrendanJones44 <bpj1651@rit.edu>
2018-06-17 12:24:56 -0700

    add backlog and change challenge_responses factory to always chose number 1 since a random number could generate an invalid model

--- spec/factories/challenge_responses.rb
+++ spec/factories/challenge_responses.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :challenge_response do
     odds_out_of { Faker::Number.between(10, 100) }
-    number_chosen { Faker::Number.between(1, 9) }
+    number_chosen { 1 }
     association :odds_are, factory: :odds_are
   end
 end
