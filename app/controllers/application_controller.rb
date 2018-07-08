b233d4a0fd69a7ba550c680b5d95481597ce5204
BrendanJones44 <bpj1651@rit.edu>
2018-07-08 10:52:02 -0700

    begin overhaul of front-end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # Top level controller for Rails
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    '/'
+  end
 end
