77be630d9dce66353b5db28272381c206b9377b2
BrendanJones44 <bpj1651@rit.edu>
2018-10-08 15:20:23 -0400

    add mobile menu selector

---
+++ app/assets/javascripts/mobile-menu.js
@@ -0,0 +1,19 @@
+var isShowing = false;
+function handleMenuToggle(x) {
+  x.classList.toggle("change");
+  if(isShowing) {
+    $('#left-sidebar-wrapper').hide();
+  } else {
+    $('#left-sidebar-wrapper').show();
+    $('#left-sidebar-wrapper').css('z-index', '1000');
+  }
+  isShowing = !isShowing;
+}
+$(window).resize(function () {
+  if ($(this).width() >= 676) {
+    $('#left-sidebar-wrapper').show();
+  }
+  else {
+    $('#left-sidebar-wrapper').hide();
+  }
+});
