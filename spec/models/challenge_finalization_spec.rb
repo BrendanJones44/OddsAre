f5d1f3db16f75767f98cbf622cc29b8c21fb1755
BrendanJones44 <bpj1651@rit.edu>
2019-01-11 07:46:03 -0800

    add specs to challenge_finalization#number_chosen_in_bounds

--- spec/models/challenge_finalization_spec.rb
+++ spec/models/challenge_finalization_spec.rb
@@ -38,4 +38,53 @@ RSpec.describe ChallengeFinalization, type: :model do
       }
     end
   end
+
+  describe '#number_chosen_in_bounds' do
+    context 'with number chosen being equal to what it is set out of' do
+      let(:odds_are) { odds_are_with_no_finalization }
+      let(:challenge_finalization) do
+        ChallengeFinalization.create(number_guessed: odds_are.odds_out_of,
+                                     odds_are_id: odds_are.id)
+      end
+      it {
+        expect(challenge_finalization.errors[:number_guessed].first)
+          .to eq 'Your number must be less than what the odds are out of'
+      }
+    end
+
+    context 'with number chosen being greater than what it is set out of' do
+      let(:odds_are) { odds_are_with_no_finalization }
+      let(:challenge_finalization) do
+        ChallengeFinalization.create(number_guessed: odds_are.odds_out_of + 2,
+                                     odds_are_id: odds_are.id)
+      end
+      it {
+        expect(challenge_finalization.errors[:number_guessed].first)
+          .to eq 'Your number must be less than what the odds are out of'
+      }
+    end
+
+    context 'with number chosen being smaller than 1' do
+      let(:odds_are) { odds_are_with_no_finalization }
+      let(:challenge_finalization) do
+        ChallengeFinalization.create(number_guessed: 0,
+                                     odds_are_id: odds_are.id)
+      end
+      it {
+        expect(challenge_finalization.errors[:number_guessed].first)
+          .to eq 'Your number must be at least 1'
+      }
+    end
+
+    context 'with number chosen being in range' do
+      let(:odds_are) { odds_are_with_no_finalization }
+      let(:challenge_finalization) do
+        ChallengeFinalization.create(number_guessed: odds_are.odds_out_of - 1,
+                                     odds_are_id: odds_are.id)
+      end
+      it {
+        expect(challenge_finalization).to be_valid
+      }
+    end
+  end
 end
