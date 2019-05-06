b49138b6ae7192dd842cd58a473c51a96b3daab4
BrendanJones44 <bpj1651@rit.edu>
2019-05-05 20:19:37 -0700

    add back migrations

--- db/migrate/20190506024037_add_auth_uuid_to_users.rb
+++ db/migrate/20190506024037_add_auth_uuid_to_users.rb
@@ -1,5 +1,5 @@
 class AddAuthUuidToUsers < ActiveRecord::Migration[5.0]
   def change
-    #add_column :users, :auth_uuid, :uuid, default: "gen_random_uuid()", null: false
+    add_column :users, :auth_uuid, :uuid, default: "gen_random_uuid()", null: false
   end
 end
